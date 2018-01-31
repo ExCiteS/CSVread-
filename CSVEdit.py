96ec9cdac68a5b0678d73937f3636aa9b36d977b
Julia Altenbuchner <julia.altenbuchner@ucl.ac.uk>
2018-01-31 15:29:47 +0000

    Auto-detect value and location fields

--- CSVEdit.py
+++ CSVEdit.py
@@ -1,16 +1,20 @@
 import csv
 import os
 from tkinter.filedialog import askopenfilename
-#CHANGE NEXT LINE TO CONTROL WHICH FIELDS ARE PRINTED
-fieldsToKeep = ["StartTime-LocalYYYYMMDD_HHMMSS", "EndTime-LocalYYYYMMDD_HHMMSS", "Situation-Value", "Location.Latitude", "Location.Longitude", "Location.Altitude", "Location.Bearing", "Location.Accuracy"]
-#End change
 
-fileName, fileExtension = os.path.splitext(askopenfilename( filetypes=[("Sapelli CSV","*.csv")]));
-oldFile = fileName + fileExtension;
-newFile = fileName + '_new' + fileExtension;
+## Modify this line to change logic of fields to keep
+keepers = ("StartTime-LocalYYYYMMDD_HHMMSS", "EndTime-LocalYYYYMMDD_HHMMSS", "-Value", ".Latitude", ".Longitude", ".Accuracy")
+
+fileName, fileExtension = os.path.splitext(askopenfilename(filetypes=[("Sapelli CSV", "*.csv")]))
+oldFile = fileName + fileExtension
+newFile = fileName + '_new' + fileExtension
+fieldsToKeep = []
 
 with open(oldFile) as infile, open(newFile, "w", newline="") as outfile:
     r = csv.DictReader(infile)
+    for fieldName in r.fieldnames:
+        if fieldName.endswith(keepers):
+            fieldsToKeep.append(fieldName)
     w = csv.DictWriter(outfile, fieldsToKeep, extrasaction="ignore")
     w.writeheader()
     for row in r:
